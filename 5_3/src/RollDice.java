908aaa03775733901ea0c7dbd447b52e71c510ba
bartosz-pikutin <s24819@pjwstk.edu.pl>
2025-04-26 20:46:39 +0200

    Wprowadzenie nowej klasy z zadania

---
+++ 5_3/src/RollDice.java
@@ -0,0 +1,20 @@
+import java.util.Iterator;
+
+class Roll implements Iterable<Integer>{
+
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return null;
+    }
+}
+
+public class RollDice {
+    public static void main(String[] args) {
+        for (int turn = 0; turn < 10; ++turn) {
+            for (Integer i : new Roll())
+                System.out.print(i + " ");
+            System.out.println();
+        }
+    }
+}
