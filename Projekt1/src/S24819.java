d2a7040abad894e1ea8c01a61402d2835407f265
bartek <s24819@pjwstk.edu.pl>
2025-05-15 14:38:58 +0200

    Zmiana z try-catch na switch case

--- Projekt1/src/S24819.java
+++ Projekt1/src/S24819.java
@@ -8,16 +8,23 @@ public class S24819 {
         System.out.println("1. Int" + "\n2. Double" + "\n3. Person (birth year and name)" + "\n4. MyColor 9 R, G, B and Sum)" +
                 "\n5.Car (make, model)" + "\n6.Smth\n"); //do wymyślenia
 
-        while (true) {
+        dataType selectedDataType = null;
+        while (selectedDataType == null) {
             Scanner sc = new Scanner(System.in);
-            String choice = sc.nextLine();
-            try {
-                dataType type = dataType.valueOf(choice);
-                break;
+            String choice = sc.nextLine().trim().toUpperCase();
 
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage() + "\nPlease enter a valid data type: \n");
-            }
+            selectedDataType = switch (choice) {
+                case "1", "INT" -> dataType.INT;
+                case "2", "DOUBLE" -> dataType.DOUBLE;
+                case "3", "PERSON" -> dataType.PERSON;
+                case "4", "MYCOLOR" -> dataType.MYCOLOR;
+                case "5", "CAR" -> dataType.CAR;
+                case "6", "SMTH" -> dataType.SMTH;
+                default -> {
+                    System.out.println("Invalid input. Try again.");
+                    yield null;
+                }
+            };
         }
 
         System.out.println("Choose a number of data to be generated: \n1. 100\n2. 500\n3. 1000\n4. 10000\n5. other value");
@@ -26,7 +33,7 @@ public class S24819 {
 
 
 enum dataType{
-    Int,Double,Person,MyColor, Car, Smth;
+    INT,DOUBLE,PERSON,MYCOLOR, CAR, SMTH;
 }
 
 enum collectionType {
